0574ee18f7fac966f37432ba2dc9e45d32b3c149
Eze323 <a.ezequielirace@gmail.com>
2023-07-16 03:51:20 -0300

    update model

--- api/src/db.js
+++ api/src/db.js
@@ -3,8 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY} = process.env;
-const Dog =require('./models/Dog');
-const Temperament = require('./models/Temperament');
+
 /*
  const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
    logging: false, // set to console.log to see the raw SQL queries
